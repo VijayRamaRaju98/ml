4efdd7e2835d9bb39b60d2514d1f7557f2564290
Vijaya Rama Raju Uppalapati <uppalapativijay98@gmail.com>
2024-01-06 22:41:54 +0530

    data ingestion completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,20 +1,45 @@
-import sys
-import pandas as pd
-from dataclasses import dataclass
 import os
+import sys
+from exception import CustomException
+from logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
 
 
 
 @dataclass
 class DataIngestionConfig:
     train_data_path:str = os.path.join('artifacts', 'train.csv')
-    test_data_path:str = os.path.join('artifacts', 'test.csv')
-    data_path:str = os.path.join('artifacts', 'data.csv')
+    test_data_path:str = os.path.join('artifacts','test.csv')
+    raw_data_path:str= os.path.join('artifacts', 'data.csv')
 
 
-class Initiate_Data_ingestion():
+class DataIngestion:
     def __init__(self):
-        self.data_ingestion_config =DataIngestionConfig()
+        self.data_ingestion_config = DataIngestionConfig()
 
-    def initiate_data_ingetion(self):
-        data = pd.read_csv()
+    def initiat_data_ingestion(self):
+        logging.info('entered into the data ingestion component')
+        try:
+            df = pd.read_csv('notebook\data.csv')
+            logging.info('Read the dataset as DataFrame')
+            os.makedirs(os.path.dirname(self.data_ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.data_ingestion_config.train_data_path,index=False,header=True)
+            logging.info('train test split initiated')
+            train_set, test_set = train_test_split(df,test_size=0.2,random_state=32)
+            train_set.to_csv(self.data_ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.data_ingestion_config.test_data_path, index=False, header = True)
+            logging.info('Ingestion of the data is completed')
+
+            return (self.data_ingestion_config.train_data_path,
+                     self.data_ingestion_config.test_data_path)
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+
+if __name__=='__main__':
+    obj = DataIngestion()
+    train, test = obj.initiat_data_ingestion()
--- test.py
+++ test.py
@@ -1,18 +1,36 @@
-from typing import List
+
+
+# def solution(s:str, t:str):
+#     if len(s) != len(t):
+#         return False
+#     else:
+#         hashmaps = {}
+#         hashmapt = {}
+#         for i in range(len(s)):
+#             hashmaps[s[i]] = 1+hashmaps.get(s[i],0)
+#             hashmapt[t[i]] = 1+hashmapt.get(t[i],0)
+#         if hashmaps == hashmapt:
+#             return True
+#         else:
+#             return False
+        
+# print(solution('ant', 'tnx'))
+
+
 
 class Solution:
-    def containsduplicate(self, nums:List[int])-> bool:
-        n = nums.sort()
-        for i in range(1, len(nums)):
-            if n[i]==[n[i-1]]:
-                print(nums)
-                return True
-                break
-        else:
+    def isanagram(self, s:str,t:str):
+        if len(s) != len(t):
             return False
+        
+        char_map = [0] * 26
 
+        for i in range(len(s)):
+            char_map[ord(s[i]) - ord('a')] += 1
+            char_map[ord(t[i]) - ord('a')] -= 1
+            
 
-nums = [2,3,5,6,2]
-
-c = Solution()
-print(c.containsduplicate(nums=nums))
+        return all(count == 0 for count in char_map)
+    
+sol = Solution()
+print(sol.isanagram('anagram','nagaram'))
